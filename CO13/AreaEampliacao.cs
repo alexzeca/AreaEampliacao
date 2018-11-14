ceccb5f8f4eb554435a66f5f06fce5c817a7ca4a
Alexandre Mendes <alexandre.p.mendes@gmail.com>
2018-11-14 23:12:52 +0000

    Add project files.

---
+++ CO13/AreaEampliacao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CO13
+{
+    class AreaEampliacao
+    {
+        static void Main(string[] args)
+        {
+            Triangulo T = new Triangulo(3, 6);
+            Console.WriteLine("Area = {0}", T.Area());
+            double[] Med = new double[2];
+            Med = T.Ampliacao(3);
+
+            Console.WriteLine("Base Ampliada = {0}", Med[0]);
+            Console.WriteLine("Altura Ampliada = {0}", Med[1]);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CO13/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CO13
+{
+    class Triangulo
+    {
+        private double Base;
+        private double Altura;
+
+        public Triangulo()
+        {
+            Base = 0;
+            Altura = 0;
+        }
+
+        public Triangulo(double B, double H)
+        {
+            Base = B;
+            Altura = H;
+        }
+
+        public double Area()
+        {
+            return Base * Altura / 2;
+        }
+
+        public double [] Ampliacao(double x)
+        {
+            double[] Medidas = new double[2];
+            Medidas[0] = Base * x;
+            Medidas[1] = Altura * x;
+            return Medidas;
+        }
+    }
+}
